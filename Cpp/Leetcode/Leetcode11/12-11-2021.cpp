025e9f63999e4b4190518824e36999759503f5dc
Joshua Ramayrat <jramayrat@ad.corp.google.com>
2021-12-11 20:42:07 -0800

    leet 11

---
+++ Cpp/Leetcode/Leetcode11/12-11-2021.cpp
@@ -0,0 +1,27 @@
+#include <stdio>
+#include <vector>
+
+class Solution 
+{
+    public:
+        /*
+            Description - Finds the maximum area of water that can be held.
+            @param - vector of columns.
+            @return - max area
+        */
+        int maxArea(vector<int>& height);
+};
+
+int Solution::maxArea(vector<int>& height)
+{
+        
+}
+
+int main()
+{
+
+    Solution sol1;
+
+    vector<int> 
+
+}
